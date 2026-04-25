8ff226b9fc95ef0afd2e81526f067f5f2eefd71e
nakornjornsamoe-dotcom <nakornjornsamoe@gmail.com>
2026-04-25 20:10:27 +0700

    Add files via upload

--- cell02/ex2.2/password.py
+++ cell02/ex2.2/password.py
@@ -1,1 +1,14 @@
-
+print("Enter the frist number:")
+a=int(input())
+print("Enter the second number:")
+b=int(input())
+r=a*b
+if r<0:
+    print(a,"x",b,"=",r)
+    print("The result is negative.")
+elif r>0:
+    print(a,"x",b,"=",r)
+    print("The result is positive.")
+else:
+    print(a,"x",b,"=",r)
+    print("The result is positive and negative.")
