8fbe7b4b5c1a804aac6c0d972cd3999d5e596e5e
nakornjornsamoe-dotcom <nakornjornsamoe@gmail.com>
2026-04-25 20:19:57 +0700

    Add files via upload

--- cell02/ex2.3/mult.py
+++ cell02/ex2.3/mult.py
@@ -1,1 +1,12 @@
-
+print("Enter the first number: ")
+a=int(input)
+print("Enter the second number: ")
+b=int(input)
+r=a*b
+print(a,"x",b,"=",r)
+if r>0:
+    print("The result is positive.")
+elif r<0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
